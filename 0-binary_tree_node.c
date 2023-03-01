135ccf3415ad1f4cf79e2f90f2d10781c16fbc8b
Caesar-12 <idowuenoch65@gmail.com>
2023-03-01 09:44:47 -0800

    Add nodes to a binary tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - function to create a binary tree node
+ *
+ * @parent: Root of sub-tree or main root
+ * @value: Data to store in tree node
+ *
+ * Return: Pointer to new node or NULL if fail
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tmp, *trav;
+
+	tmp = (binary_tree_t *)malloc(sizeof(binary_tree_t *));
+	if (!(tmp))
+		return NULL;
+
+	tmp->n = value;
+	tmp->left = tmp->right = NULL;
+
+	if (parent == NULL)
+	{
+		tmp->parent = NULL;
+		return tmp;
+	}
+	else
+	{
+		tmp->parent = parent;
+		return tmp;
+	}
+}
