f0f63cedf9dfdc664e518a2213aa7dc2ade89cb0
Caesar-12 <idowuenoch65@gmail.com>
2023-03-03 04:06:08 -0800

    Measures depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,15 +10,17 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	binary_tree_t *trav;
 	size_t depth;
 
+	trav = tree;
 	depth = 0;
 	if (!tree)
 		return (0);
 
-	while (tree)
+	while (trav)
 	{
-		tree = tree->parent;
+		trav = trav->parent;
 		depth++;
 	}
 	return (depth);
