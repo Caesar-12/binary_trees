1b3d0aee04db13c721443ea4776c18b18dd5c897
Caesar-12 <idowuenoch65@gmail.com>
2023-03-03 12:06:10 -0800

    counts nodes with one child or more

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,8 +18,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (!tree->left && !tree->right)
 		node = 0;
-	node += binary_tree_node(tree->left);
-	node += binary_tree_node(tree->right);
+	node += binary_tree_nodes(tree->left);
+	node += binary_tree_nodes(tree->right);
 
 	return (node);
 }
