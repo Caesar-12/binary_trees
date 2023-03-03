8a061cf60131f72360ed51f4b19f41f1706ab679
Caesar-12 <idowuenoch65@gmail.com>
2023-03-03 03:50:49 -0800

    Measures depth and height

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measures the depth of a node
+ *
+ * @tree: pointer to the node
+ *
+ * Return: Depth of node
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	depth = 0;
+	if (!tree)
+		return (0);
+
+	while (tree)
+	{
+		tree = tree->parent;
+		depth++;
+	}
+	return (depth);
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a tree
+ *
+ * @tree: pointer to root of tree
+ *
+ * Return: height of tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+}
+
